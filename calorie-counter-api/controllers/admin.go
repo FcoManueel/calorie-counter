44150e2ced0d4b4fd8f205b7a37ae81b844fd047
Manuel Valle <FcoManueel@gmail.com>
2016-04-03 12:12:55 -0700

    Define admin controller functions

--- calorie-counter-api/controllers/admin.go
+++ calorie-counter-api/controllers/admin.go
@@ -2,6 +2,7 @@ package controllers
 
 import (
 	"fmt"
+	"goji.io/pat"
 	"golang.org/x/net/context"
 	"net/http"
 )
@@ -10,4 +11,20 @@ type Admin struct{}
 
 func (a *Admin) GetUsers(ctx context.Context, w http.ResponseWriter, req *http.Request) {
 	fmt.Fprint(w, "Admin GetUsers")
+}
+
+func (a *Admin) CreateUser(ctx context.Context, w http.ResponseWriter, req *http.Request) {
+	fmt.Fprint(w, "Admin CreateUser")
+}
+func (a *Admin) GetUser(ctx context.Context, w http.ResponseWriter, req *http.Request) {
+	userID := pat.Param(ctx, "user_id")
+	fmt.Fprint(w, "Admin GetUser. ID: %s", userID)
+}
+func (a *Admin) UpdateUser(ctx context.Context, w http.ResponseWriter, req *http.Request) {
+	userID := pat.Param(ctx, "user_id")
+	fmt.Fprint(w, "Admin UpdateUser. ID: %s", userID)
+}
+func (a *Admin) DisableUser(ctx context.Context, w http.ResponseWriter, req *http.Request) {
+	userID := pat.Param(ctx, "user_id")
+	fmt.Fprint(w, "Admin DisableUser. ID: %s", userID)
 }
