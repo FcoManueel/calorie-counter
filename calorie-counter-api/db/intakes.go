0b008cddc14de77809223913f8e209c5b9b7509d
Manuel Valle <FcoManueel@gmail.com>
2016-04-03 14:30:17 -0700

    Add repository operations for intakes

--- calorie-counter-api/db/intakes.go
+++ calorie-counter-api/db/intakes.go
@@ -1,9 +1,19 @@
 package db
 
-import "github.com/FcoManueel/calorie-counter/calorie-counter-api/models"
+import (
+	"errors"
+	"fmt"
+	"github.com/FcoManueel/calorie-counter/calorie-counter-api/models"
+	"log"
+	"time"
+)
 
 type IntakeRepository interface {
+	Get(userID, intakeID string) (*models.Intake, error)
 	GetAll(userID string) (models.Intakes, error)
+	Create(intake *models.Intake) (*models.Intake, error)
+	Update(intake *models.Intake) error
+	Disable(intakeID, userID string) error
 }
 
 var Intakes IntakeRepository
@@ -14,6 +24,59 @@ func init() {
 	Intakes = &intakeRepository{}
 }
 
+const intakeColumns = `id, user_id, name, calories, created_at, consumed_at, disabled_at`
+
+func (i *intakeRepository) Get(userID, intakeID string) (*models.Intake, error) {
+	log.Println("[intakes-get-one]", "UserID:", userID, "IntakeID:", intakeID)
+	intake := models.Intake{}
+	if _, err := db.QueryOne(&intake, fmt.Sprintf(`SELECT %s FROM intakes WHERE user_id=? AND id=? AND disabled_at IS NULL`, intakeColumns), userID, intakeID); err != nil {
+		return nil, err
+	}
+	return &intake, nil
+}
+
 func (i *intakeRepository) GetAll(userID string) (models.Intakes, error) {
-	return nil, nil
+	log.Println("[intakes-get-all]", "UserID:", userID)
+	intakes := make(models.Intakes, 0)
+	if _, err := db.Query(&intakes, fmt.Sprintf(`SELECT %s FROM intakes WHERE user_id=? AND disabled_at IS NULL`, intakeColumns), userID); err != nil {
+		return nil, err
+	}
+	return intakes, nil
+}
+
+func (i *intakeRepository) Create(intake *models.Intake) (*models.Intake, error) {
+	log.Println("[intakes-create]", "UserID:", intake.UserID, "Name:", intake.Name, "Calories:", intake.Calories)
+	if intake.ID == "" {
+		intake.ID = NewUUID()
+	} else if !IsUUID(intake.ID) {
+		return nil, errors.New(fmt.Sprintf("Invalid intake ID: %s", intake.ID))
+	}
+	intake.CreatedAt = time.Now()
+	intake.DisabledAt = nil
+
+	_, err := db.ExecOne(`INSERT INTO intakes (id, user_id, name, calories, created_at, consumed_at, disabled_at)
+	VALUES (?id, ?user_id, ?name, ?calories, now(), ?consumed_at, NULL)`, intake)
+	if err != nil {
+		return nil, err
+	}
+	return intake, nil
+}
+
+func (i *intakeRepository) Update(intake *models.Intake) error {
+	log.Println("[intakes-update]", "ID:", intake.ID)
+
+	query := `UPDATE intakes SET name=?name, calories=?calories, consumed_at=?consumed_at
+		WHERE id = ?id AND disabled_at IS NULL`
+	if _, err := db.ExecOne(query, intake); err != nil {
+		return err
+	}
+	return nil
+}
+
+func (i *intakeRepository) Disable(intakeID, userID string) error {
+	log.Println("[intakes-disable]", "ID:", intakeID, "UserID:", userID)
+	if _, err := db.ExecOne(`UPDATE intakes SET disabled_at = now() WHERE id=? AND user_id=?`, intakeID, userID); err != nil {
+		return err
+	}
+	return nil
 }
