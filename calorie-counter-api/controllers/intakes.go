1a3ef1cac73e98978ff7fc1646a6bc87eda1add4
Manuel Valle <FcoManueel@gmail.com>
2016-04-03 12:13:05 -0700

    Define intakes controller functions

--- calorie-counter-api/controllers/intakes.go
+++ calorie-counter-api/controllers/intakes.go
@@ -2,6 +2,7 @@ package controllers
 
 import (
 	"fmt"
+	"goji.io/pat"
 	"golang.org/x/net/context"
 	"net/http"
 )
@@ -10,4 +11,18 @@ type Intakes struct{}
 
 func (a *Intakes) GetAll(ctx context.Context, w http.ResponseWriter, req *http.Request) {
 	fmt.Fprint(w, "Intakes GetAll")
+}
+
+func (a *Intakes) Create(ctx context.Context, w http.ResponseWriter, req *http.Request) {
+	fmt.Fprint(w, "Intakes Create")
+}
+
+func (a *Intakes) Update(ctx context.Context, w http.ResponseWriter, req *http.Request) {
+	intakeID := pat.Param(ctx, "id")
+	fmt.Fprint(w, fmt.Sprintf("Intakes Update. ID: %s", intakeID))
+}
+
+func (a *Intakes) Disable(ctx context.Context, w http.ResponseWriter, req *http.Request) {
+	intakeID := pat.Param(ctx, "id")
+	fmt.Fprint(w, fmt.Sprintf("Intakes Disable. ID: %s", intakeID))
 }
