bf4d5beae1bb7437f0e43e2afd3bd8ed7f6d161a
Manuel Valle <FcoManueel@gmail.com>
2016-04-03 00:32:39 -0700

    Add mechanism to apply database migrations

--- calorie-counter-api/main.go
+++ calorie-counter-api/main.go
@@ -1,6 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/FcoManueel/calorie-counter/calorie-counter-api/db"
+	"log"
+)
+
+func init() {
+	if err := db.Migrate("gouser", "gouser", "127.0.0.1", "calories_dev"); err != nil {
+		log.Fatal("[db-migrate] Error while applying migrations: ", err.Error())
+	}
+}
 
 func main() {
 	fmt.Println("Hello")
