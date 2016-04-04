af0ed7a1dbc13851cf852be439d82388f76f8be8
Manuel Valle <FcoManueel@gmail.com>
2016-04-03 23:40:35 -0700

    Update routes to account for API version (v1) and BearerAuth middleware

--- calorie-counter-api/routes/routes.go
+++ calorie-counter-api/routes/routes.go
@@ -18,33 +18,33 @@ func Init() *goji.Mux {
 	authMux.HandleFuncC(pat.Post("/signup"), authCtrl.Signup)
 	authMux.HandleFuncC(pat.Post("/login"), authCtrl.Login)
 
+	v1 := goji.SubMux()
+	v1.UseC(middleware.BearerAuth)
+	root.HandleC(pat.New("/v1/*"), v1)
+
 	// users routes
-	usersMux := goji.SubMux()
-	root.HandleC(pat.New("/users/*"), usersMux)
 	usersCtrl := controllers.Users{}
-	usersMux.HandleFuncC(pat.Get("/:id"), usersCtrl.Get)
-	usersMux.HandleFuncC(pat.Put("/:id"), usersCtrl.Update)
-	usersMux.HandleFuncC(pat.Delete("/:id"), usersCtrl.Disable)
+	v1.HandleFuncC(pat.Get("/users/:id"), usersCtrl.Get)
+	v1.HandleFuncC(pat.Put("/users/:id"), usersCtrl.Update)
+	v1.HandleFuncC(pat.Delete("/users/:id"), usersCtrl.Disable)
 
 	// intakes routes
-	intakesMux := goji.SubMux()
-	root.HandleC(pat.New("/intakes/*"), intakesMux)
 	intakesCtrl := controllers.Intakes{}
-	intakesMux.HandleFuncC(pat.Post("/"), intakesCtrl.Create)
-	intakesMux.HandleFuncC(pat.Get("/"), intakesCtrl.GetAll)
-	intakesMux.HandleFuncC(pat.Get("/:id"), intakesCtrl.Get)
-	intakesMux.HandleFuncC(pat.Put("/:id"), intakesCtrl.Update)
-	intakesMux.HandleFuncC(pat.Delete("/:id"), intakesCtrl.Disable)
+	v1.HandleFuncC(pat.Post("/intakes"), intakesCtrl.Create)
+	v1.HandleFuncC(pat.Get("/intakes"), intakesCtrl.GetAll)
+	v1.HandleFuncC(pat.Get("/intakes/:id"), intakesCtrl.Get)
+	v1.HandleFuncC(pat.Put("/intakes/:id"), intakesCtrl.Update)
+	v1.HandleFuncC(pat.Delete("/intakes/:id"), intakesCtrl.Disable)
 
 	// intakes routes
-	adminMux := goji.SubMux()
-	root.HandleC(pat.New("/admin/*"), adminMux)
+	admin := goji.SubMux()
+	root.HandleC(pat.New("/admin/*"), admin)
 	adminCtrl := controllers.Admin{}
-	adminMux.HandleFuncC(pat.Get("/users"), adminCtrl.GetUsers)
-	adminMux.HandleFuncC(pat.Post("/users"), adminCtrl.CreateUser)
-	adminMux.HandleFuncC(pat.Get("/users/:user_id"), adminCtrl.GetUser)
-	adminMux.HandleFuncC(pat.Put("/users/:user_id"), adminCtrl.UpdateUser)
-	adminMux.HandleFuncC(pat.Delete("/users/:user_id"), adminCtrl.DisableUser)
+	admin.HandleFuncC(pat.Get("/users"), adminCtrl.GetUsers)
+	admin.HandleFuncC(pat.Post("/users"), adminCtrl.CreateUser)
+	admin.HandleFuncC(pat.Get("/users/:user_id"), adminCtrl.GetUser)
+	admin.HandleFuncC(pat.Put("/users/:user_id"), adminCtrl.UpdateUser)
+	admin.HandleFuncC(pat.Delete("/users/:user_id"), adminCtrl.DisableUser)
 
 	/*
 	   //	For later consideration:
